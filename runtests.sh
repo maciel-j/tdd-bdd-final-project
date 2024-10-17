a01d48d43f780154cd82c93f3f0364612c691977
jmaciel38 <josemaciel30@gmail.com>
2024-10-17 07:37:35 -0300

    FEAT: append on top 2024.10.17

--- runtests.sh
+++ runtests.sh
@@ -2,15 +2,35 @@
 timestamp=$(date '+%Y.%m.%d-%H:%M:%S')
 runtime_timestamp="********************* [${timestamp}] *********************\n\n"
 
+NOSETESTS_FILE="nosetests.txt"
+NOSETESTS_TEMP_FILE=$(mktemp)
+
+LINT_FILE="lint.txt"
+LINT_TEMP_FILE=$(mktemp)
+
 function runNosetests () {
-    printf "${runtime_timestamp}" >> nosetests.txt
-    nosetests -s >> nosetests.txt 2>&1;
+    printf "${runtime_timestamp}" > "${NOSETESTS_TEMP_FILE}"
+    nosetests -s >> "${NOSETESTS_TEMP_FILE}" 2>&1;
+    if [ -e "${NOSETESTS_FILE}" ]; then
+        cat "${NOSETESTS_FILE}" >> "${NOSETESTS_TEMP_FILE}"
+    else
+        touch "${NOSETESTS_FILE}"
+        cat "${NOSETESTS_FILE}" >> "${NOSETESTS_TEMP_FILE}"
+    fi
+    mv "${NOSETESTS_TEMP_FILE}" "${NOSETESTS_FILE}"
 }
 
 function runMakeLint () {
-    printf "${runtime_timestamp}" >> lint.txt
-    make lint -s >> lint.txt 2>&1;
-    printf "\n" >> lint.txt
+    printf "${runtime_timestamp}" >> "${LINT_TEMP_FILE}"
+    make lint -s >> "${LINT_TEMP_FILE}" 2>&1;
+    printf "\n" >> "${LINT_TEMP_FILE}"
+    if [ -e "${LINT_FILE}" ]; then
+        cat "${LINT_FILE}" >> "${LINT_TEMP_FILE}"
+    else
+        touch "${LINT_FILE}"
+        cat "${LINT_FILE}" >> "${LINT_TEMP_FILE}"
+    fi
+    mv "${LINT_TEMP_FILE}" "${LINT_FILE}"
 }
 
 runNosetests
